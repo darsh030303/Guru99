fd720340f6e3878d3f23af04fcb7fbb2e3d2a821
Kajal <Kajal@Kajal-PC>
2020-04-14 21:57:11 +0530

    trialll1

---
+++ Guru99Test/src/main/java/pages/HomePage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+	WebDriver driver;
+
+	   
+  //  @FindBy(xpath="//table//tr[@class='heading3']")  WebElement homePageUserName;
+
+    public HomePage(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    //Get the User name from Home Page
+    public String getHomePagetitle()
+    {
+         return     driver.getTitle();
+    }
+}
---
+++ Guru99Test/src/test/java/test/SanityTest.java
@@ -0,0 +1,73 @@
+package test;
+
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+import pages.LoginPage;
+
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class SanityTest {
+	
+	WebDriver driver;
+	
+	LoginPage objLogin;
+    HomePage objHomePage; //first comment for first commit    sss
+ 
+  @BeforeTest
+  public void beforeTest()
+  {
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://demo.guru99.com/V4/");	
+  }
+
+  @Test(priority=0)
+  public void test_MainPage_Check()
+  {
+	  //Create Login Page object
+	  objLogin = new LoginPage(driver);
+	  
+	  //Verify login page title
+	    String loginPageTitle = objLogin.getLoginTitle();
+	    Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+	    
+		    
+	
+  }
+  
+  @Test(priority=1)
+  public void login()
+  {
+	//login to application
+	    objLogin.loginToGuru99("mngr252548", "buvEnat");
+  }
+  
+  @Test(priority=2)
+  public void afterLogin()
+  {
+	  // go the next page
+	    objHomePage = new HomePage(driver);
+	    
+	   // System.out.println(objHomePage.getHomePagetitle());
+
+	  //Verify home page
+	    Assert.assertTrue(objHomePage.getHomePagetitle().contains("Guru99 Bank Manager HomePage"));
+	    
+	    System.out.println("Homepage test pass");
+  }
+
+  @AfterTest
+  public void afterTest()
+  {
+	  driver.close();
+  }
+}
